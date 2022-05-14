ae2507f065ce5119e6ca56e8a069c164b24a5f81
slavo <slavomirholenda@gmail.com>
2022-05-14 21:02:44 +0200

    binary inputs for MLP

--- converg_xor_hidden.py
+++ converg_xor_hidden.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from expnet_numpy import ExpNet
 from perceptron_numpy import Perceptron
 from net_util import Exp, Tahn, SigmoidNp
-from generator import paritaMinus
+from generator import paritaMinus, parita
 from converg import convergencia
 from statistics import mean, stdev
 
@@ -13,7 +13,8 @@ tahn = Tahn()
 sigmoid = SigmoidNp()
 
 p = 2
-inputs, labels = paritaMinus(p)
+inputs_minus, labels_minus = paritaMinus(p)
+inputs_binary, labels_binary = parita(p)
 learning_rate = 0.5
 max_epoch = 1000
 repetitions = 10
@@ -24,7 +25,7 @@ results = {}
 for h in hidden_size:
     print("Testing hidden size: {}".format(h))
     architecture = [p, h, 1]
-    results_expnet = convergencia(architecture, ExpNet, [tahn, exp], learning_rate, max_epoch, repetitions, success_window, inputs, labels, False)
+    results_expnet = convergencia(architecture, ExpNet, [tahn, exp], learning_rate, max_epoch, repetitions, success_window, inputs_minus, labels_minus, False)
     print("EXP nets: {}/{} in {} +- {} epochs. Runtime: {:.1f}s".format(
         results_expnet["nets"],
         repetitions,
@@ -32,7 +33,7 @@ for h in hidden_size:
         stdev(results_expnet["epochs"]),
         results_expnet["time"]
     ))
-    results_mlp = convergencia(architecture, Perceptron, [sigmoid, sigmoid], learning_rate, max_epoch, repetitions, success_window, inputs, labels, False)
+    results_mlp = convergencia(architecture, Perceptron, [sigmoid, sigmoid], learning_rate, max_epoch, repetitions, success_window, inputs_binary, labels_binary, False)
     print("MLP nets: {}/{} in {} +- {} epochs. Runtime: {:.1f}s".format(
         results_mlp["nets"],
         repetitions,
